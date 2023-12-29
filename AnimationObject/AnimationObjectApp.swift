3316c2b0253f3671de164a0aacda59441c2dcb3b
Gridenko Maks <Maksgridenko@ya.ru>
2023-12-29 16:34:30 +0300

    Initial Commit

---
+++ AnimationObject/AnimationObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationObjectApp.swift
+//  AnimationObject
+//
+//  Created by Maxim Gridenko on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
